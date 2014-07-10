c08be0b1994079c770516d73f6fec5b399c5a633
Fatima <fatima.nadine@gmail.com>
2014-07-09 21:45:30 -0400

    added active admin stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require turbolinks
 //= require jquery
+//= require jquery.ui.datepicker
+//= require jquery-ui
 //= require jquery_ujs
 //= require bootstrap
-//= require turbolinks
 //= require_tree .
