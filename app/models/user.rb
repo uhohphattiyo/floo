dcc9d3f469ceeff0848b36d8b3cb07117ffb1659
Fatima <fatima.nadine@gmail.com>
2014-07-18 17:05:40 -0400

    added bday and gender

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   
+   validates :birthdate, presence: true
 
 end
