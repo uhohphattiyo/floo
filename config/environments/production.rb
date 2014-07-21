13fa64c3ad0d3e529da7cf43790153ea013bcf95
Fatima <fatima.nadine@gmail.com>
2014-07-21 19:33:45 -0400

    facebook auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
     # Email
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => config.app_domain }
+  config.action_mailer.default_url_options = { :host => config.swille.com }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com', 
     port: '587',
