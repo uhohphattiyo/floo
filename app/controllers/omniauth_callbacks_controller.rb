7e32951a723fa6060f003140e147c61024385cfd
Fatima <fatima.nadine@gmail.com>
2014-07-22 17:57:55 -0400

    fb auth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,21 +1,18 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def self.provides_callback_for(provider)
-    class_eval %Q{
-      def #{provider}
-        @user = User.find_for_oauth(env["omniauth.auth"], current_user)
+ 
+  def facebook
+    user = User.find_for_oauth(request.env["omniauth.auth"])
 
-        if @user.persisted?
-          sign_in_and_redirect @user, event: :authentication
-          set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-        else
-          session["devise.#{provider}_data"] = env["omniauth.auth"]
-          redirect_to new_user_registration_url
-        end
-      end
-    }
+    if user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      sign_in_and_redirect user, :event => :authentication
+    else
+      session["devise.github_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
   end
 
-  [:twitter, :facebook, :linked_in].each do |provider|
+  [:facebook].each do |provider|
     provides_callback_for provider
   end
 
