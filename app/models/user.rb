0ccab3cacd2dea6103c87f8e7a1a37ddd75ac5c0
Fatima <fatima.nadine@gmail.com>
2014-07-22 18:26:28 -0400

    fb auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
 
 
-
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
@@ -17,7 +15,7 @@ class User < ActiveRecord::Base
     end
   end
 
-  class User < ActiveRecord::Base
+
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
