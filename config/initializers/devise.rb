cdecff7277227fc4747da33d6fa14dd09c48d9ee
Fatima <fatima.nadine@gmail.com>
2014-07-22 16:34:31 -0400

    fb auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -254,4 +254,4 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-   config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+   config.omniauth = {:facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'] }
