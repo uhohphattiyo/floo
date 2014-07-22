a20c92e4abaf81f7586b261b204759733e6e4818
Fatima <fatima.nadine@gmail.com>
2014-07-21 22:50:51 -0400

    fb auth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # For Devise / Heroku
-  config.action_mailer.default_url_options = { :host => 'swille.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,19 +94,5 @@ Rails.application.configure do
     }
   }
 
-    #for omniauth facebook auth
-    # Email
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
- config.action_mailer.default_url_options = { :host => config.swille }
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com', 
-    port: '587',
-    enable_starttls_auto: true,
-    user_name: 'someuser',
-    password: 'somepass',
-    authentication => :plain,
-    domain => 'somedomain.com'
-  }
 
 end
