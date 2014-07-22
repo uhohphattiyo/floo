591fb9e1f7249afb5da92dce7a0bc97baca304f1
Fatima <fatima.nadine@gmail.com>
2014-07-22 17:44:19 -0400

    fb auth

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,13 @@ Rails.application.routes.draw do
   get "about" => "pages#about"
   get "privacy" => "pages#privacy"
 
+=begin
   devise_scope :user do
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
-  
+=end
 
-    match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+  ##  match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
 
 
