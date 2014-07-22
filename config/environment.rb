9372a7629980ac4a524d38a03fd57f226fb60b36
Fatima <fatima.nadine@gmail.com>
2014-07-21 22:49:29 -0400

    fb auth

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,20 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+
+    #for omniauth facebook auth
+    # Email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+ config.action_mailer.default_url_options = { :host => config.swille }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com', 
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: 'someuser',
+    password: 'somepass',
+    authentication => :plain,
+    domain => 'somedomain.com'
+  }
