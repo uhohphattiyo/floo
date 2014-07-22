623a9f433c96fc9e648f13d57196f351dde8afbc
Fatima <fatima.nadine@gmail.com>
2014-07-22 17:39:58 -0400

    fb auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,24 @@ class User < ActiveRecord::Base
 
     validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
 
-    def self.find_for_oauth(auth, signed_in_resource = nil)
+    def self.from_omniauth(auth)
+      where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+      end
+  end
+
+  class User < ActiveRecord::Base
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+end
 
     # Get the identity and user if they exist
     identity = Identity.find_for_oauth(auth)
--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,15 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
   get "privacy" => "pages#privacy"
+
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   
 
     match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
