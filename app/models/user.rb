6071ed37ed011b841db6fb5f2af57dbde7ca70fa
Fatima <fatima.nadine@gmail.com>
2014-07-22 23:45:48 -0400

    fb auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,6 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       end
-
-       if @user.persisted?
-        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-        set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-      else
-        session["devise.facebook_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
-      end
     end
 
 
