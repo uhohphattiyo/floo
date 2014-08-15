222c99d7c228771cafdc914236c1ee687dc295d7
Fatima <fatima.nadine@gmail.com>
2014-08-15 01:52:15 -0400

    remove gem monologue

--- config/initializers/assets.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.eot )
-Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.woff )
-Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.ttf )
