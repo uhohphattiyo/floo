6a8b1b126646adc2b141563dfbc054493540de0f
Fatima <fatima.nadine@gmail.com>
2014-08-15 01:39:30 -0400

    trying to fix heroku monologue issue

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,27 @@ module Floo
 
 
 
-Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.eot )
-Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.woff )
-Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.ttf )
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif,
+                                  "social_foundicons.eot",
+                                 "social_foundicons.woff",
+                                 "social_foundicons.ttf",
+                                 )
+
+
+
+config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    end
 end
---
+++ config/initializers/assets.rb
@@ -0,0 +1,3 @@
+Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.eot )
+Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.woff )
+Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.ttf )
