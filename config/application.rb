b132ac1781f8c7c2382bba145c6c1dd244f33c50
Fatima <fatima.nadine@gmail.com>
2015-05-30 22:12:57 -0500

    deleting dotenv

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,6 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
-HOSTNAME = ENV['HOSTNAME']
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
